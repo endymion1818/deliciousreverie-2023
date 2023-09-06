5f5dcbb6ecd50ab99605354a7683831d938ffb10
endymion1818 <endymion1818@gmail.com>
2023-09-06 21:07:53 +0100

    working build

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+const { join } = require("path")
+
+module.exports = {
+  content: [
+    join(__dirname, 'src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}'),
+  ],
+  theme: {
+    container: {
+      center: true,
+    },
+    fontFamily: {
+      'display': ['Lovechild'],
+    }
+  },
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#333",
+          secondary: "#f7ebc1",
+          accent: "#331f33",
+        },
+      },
+      "luxury",
+    ],
+  },
+}
