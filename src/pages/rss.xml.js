73efe83ec8d0b6029cd90c57b9b54bea995b9323
Ben Read <endymion1818@gmail.com>
2023-09-25 13:44:58 +0100

    fix rss feed (#1)
    * fix rss feed
    
    * again rss feed path update
    
    * 3rd attempt
    
    * fourth attempt

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,13 +1,20 @@
-import rss, { pagesGlobToRssItems } from '@astrojs/rss';
-import { title, subtitle } from "../../sitedata"
+import rss from '@astrojs/rss';
+import { title, subtitle, siteUrl } from "../../sitedata"
+import sanitizeHtml from 'sanitize-html';
 
 export async function GET(context) {
+  const postImportResult = import.meta.glob('./posts/*.md', { eager: true });
+  const posts = Object.values(postImportResult);
+  const items = posts.map((post) => ({
+    title: post.frontmatter.title,
+    description: post.frontmatter.description,
+    pubDate: post.frontmatter.datePublished,
+    link: `${siteUrl}${post.url}`,
+  }))
   return rss({
     title,
     description: subtitle,
     site: context.site,
-    items: await pagesGlobToRssItems(
-      import.meta.glob('./src/pages/posts/*.{md}'),
-    ),
+    items,
   });
 }
