5ec0db8a3ac68ba12963123d0e6b3f2a591e0f1e
endymion1818 <endymion1818@gmail.com>
2023-10-13 16:28:48 +0100

    init sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://deliciousreverie.co.uk',
   integrations: [
-    tailwind()
+    tailwind(),
+    sitemap()
   ]
 });
