c0501ffe0ad857428092ef96ac63ce8cb92f703d
Ben Read <endymion1818@gmail.com>
2024-03-21 17:37:17 +0000

    Feat comments (#22)
    * get comments
    
    * render comments
    
    * form
    
    * init function
    
    * actual working function
    
    * wip form submissions
    
    * success page
    
    * function body
    
    * a little paranoia never hurt anyone
    
    * document todo
    
    * gah
    
    * i think its working
    
    * working comment flow
    
    * working comments
    
    * add netlify staging domain to valid referrers
    
    * put back response templates
    
    * better ui for responses
    
    * rm dead code, actually use templates
    
    * better alerts
    
    * debug host
    
    * sniff nostname and add netlifyreferrer
    
    * properly match hostname
    
    * update referrers
    
    * debug invalid url
    
    * ditch url constructor
    
    * get out of jail free
    
    * wait for dom to load before looking for the form
    
    * bored now
    
    * this better be it

---
+++ netlify/functions/send-comment/send-comment.js
@@ -0,0 +1,93 @@
+/**
+ * @type {import('@netlify/functions').Handler}
+ */
+
+/**
+ * 
+ * @param {Handler.event} event 
+ * @returns 
+ */
+const handler = async (event) => {
+
+  const { host } = event.headers;
+
+  const validReferrers = ['localhost:8888', 'deliciousreverie.co.uk', 'deploy-preview-22--gleaming-melba-90c3d4.netlify.app'];
+
+  // let's just check that shall we?
+  if(!event.body) {
+    return {
+      statusCode: 302,
+      message: 'no data to send',
+    }
+  }
+
+  const payload = JSON.parse(event.body);
+  
+  if (!validReferrers.includes(host)) {
+    console.log('invalid referrer');
+    return { message: 'Invalid referrer', statusCode: 405 };
+  }
+  if(payload.body.length > 500) {
+    console.log('message too long')
+    return { message: 'Message too long', statusCode: 405 }
+  }
+  if(payload.name.length > 100) {
+    console.log('Name too long')
+    return { message: 'Name too long', statusCode: 405 }
+  }
+  if(payload.email.length > 100) {
+    console.log('Email too long')
+    return { message: 'Email too long', statusCode: 405 }
+  }
+  if(!/^[\w\-\s]+$/.test(payload.body)) {
+    console.log('dodgy message')
+    return { message: 'Looks dodgy to me', statusCode: 405 }
+  }
+  // looks legit
+  try {
+    const result = await fetch(process.env.WEBINY_API_URL, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${process.env.WEBINY_API_KEY}`
+      },
+      body: JSON.stringify({
+        query: `
+          mutation CreateCommentMutation($CommentInput: CommentInput!) {
+            createComment(data: $CommentInput) {
+              data {
+                id
+              }
+            }
+          }
+        `,
+        variables: {
+          CommentInput: {
+            name: payload.name,
+            email: payload.email,
+            body: payload.body ?? '',
+            slug: payload.slug,
+          }
+        }
+      })
+    })
+    const { data, errors } = await result.json();
+    if(errors || data.error) {
+      return {
+        message: `${errors?.map(e => e?.message).join(', ')}`,
+        statusCode: 302
+      }
+    }
+    return {
+      message: 'Comment sent',
+      statusCode: 200
+    }; 
+  } catch (error) {
+    return {
+      message: `${error.toString()}`,
+      statusCode: 302
+    }
+  }
+}
+
+module.exports = { handler }
---
+++ src/components/getComments.ts
@@ -0,0 +1,39 @@
+async function getComments() {
+    try {
+      const res = await fetch(`${import.meta.env.WEBINY_API_READ_URL}`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${import.meta.env.WEBINY_API_KEY}`
+        },
+        body: JSON.stringify({
+            query: `
+            query getComments {
+              listComments {
+                data {
+                  id
+                  body
+                  name
+                  slug
+                  createdOn
+                }
+              }
+            }
+          `,
+        })
+    });
+    const data = await res.json();
+    if(!data) {
+      console.log('No data');
+      return;
+    }
+    if(data.errors) {
+      console.log(data.errors);
+      return;
+    }
+    return data.data.listComments.data;
+    } catch (error) {
+      console.log(error);
+    }
+}
+export default getComments;
