199c995e2102c766b5e022da1b501743e6e8074b
endymion1818 <endymion1818@gmail.com>
2025-01-09 17:31:57 +0000

    feat: pagefind search

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
+import pagefind from "astro-pagefind";
 
 export default defineConfig({
   site: 'https://deliciousreverie.co.uk',
@@ -9,6 +10,7 @@ export default defineConfig({
   },
   integrations: [
     tailwind(),
+    pagefind(),
     sitemap({
       serialize(item) {
         return {
